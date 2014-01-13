d34d48db9ea1f54c87368f7bf0c63f4b6b5d3130
Git-Lucky <fraglerocked@gmail.com>
2014-01-13 09:58:01 -0800

    Initial Commit

---
+++ FirstDayDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FirstDayDemo
+//
+//  Created by Tim Hise on 1/13/14.
+//  Copyright (c) 2014 GelRock. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
